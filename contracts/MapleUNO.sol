762af8bf55f2e30206ce883aee7bd093644715f5
Rohan <RohanZarathustra@gmail.com>
2023-04-24 11:23:45 -0400

    Maple-UNO vault init

--- contracts/MapleUNO.sol
+++ contracts/MapleUNO.sol
@@ -2,6 +2,27 @@
 
 pragma solidity 0.8.7;
 
-contract MapleUNO {
+import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/**@title MapleUNO
+  *@author Rohan Nero
+  *@notice this contract allows UNO users to stake with Maple RWA asset pools at the same time as UNO SSIP pools
+  *@dev UNO's vault stores the Maple vault `shares` token as its asset */
+contract MapleUNO is ERC4626 {
+
+
+    IERC20 private immutable _asset;
     
+
+    /**@notice need to provide the asset that is used in this vault
+      *@dev an example Maple USDC pool address: 0xd3cd37a7299B963bbc69592e5Ba933388f70dc88
+      *@param asset_ the IERC contract you wish to use as the vault asset */
+    constructor(IERC20 asset_) ERC4626(asset_) ERC20("Maple UNO","mUNO") {
+      _asset = asset_;
+    }
+
+
+
+  
 }
--- deploy/mapleUNO.js
+++ deploy/mapleUNO.js
@@ -5,8 +5,6 @@ const {
 } = require("../helper-hardhat-config.js")
 const { verify } = require("../utils/verify.js")
 
-const FUND_AMOUNT = "1000000000000000000000"
-
 module.exports = async ({ deployments, getNamedAccounts }) => {
   const { deployer } = await getNamedAccounts()
   const { deploy, log } = deployments
@@ -14,18 +12,7 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
   //log(chainId)
 
   if (developmentChains.includes(network.name)) {
-    vrfCoordinator = await ethers.getContract("VRFCoordinatorV2Mock")
-    vrfCoordinatorAddress = vrfCoordinator.address
-    const transactionResponse = await vrfCoordinator.createSubscription()
-    const transactionReceipt = await transactionResponse.wait()
-    subscriptionId = transactionReceipt.events[0].args.subId
-    //log(subscriptionId.toString())
-    // Fund the subscription
-    // Our mock makes it so we don't actually have to worry about sending fund
-    await vrfCoordinator.fundSubscription(subscriptionId, FUND_AMOUNT)
   } else {
-    vrfCoordinatorAddress = networkConfig[chainId]["vrfCoordinator"]
-    subscriptionId = networkConfig[chainId]["subId"]
   }
 
   args = [
@@ -35,10 +22,11 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
     networkConfig[chainId]["callbackGaslimit"],
   ]
 
-  const jury = await deploy("VRFJury", {
+  mapleUNO = await deploy("MapleUNO", {
     from: deployer,
-    log: true,
     args: args,
+    log: true,
+    waitConfirmations: networkConfig[chainId].blockConfirmations,
   })
 
   if (!developmentChains.includes(network.name)) {
@@ -47,4 +35,4 @@ module.exports = async ({ deployments, getNamedAccounts }) => {
   }
 }
 
-module.exports.tags = ["all", "main", "jury"]
+module.exports.tags = ["all", "main", "uno"]
