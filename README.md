71d1acbcd7b824a8b7036eec476657e207dd9091
Rohan <RohanZarathustra@gmail.com>
2023-04-25 15:47:05 -0400

    enterPool structure added to Loki

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 ## UNO RWA
 
 These are designs for new UNO RE staking pools that allow users to invest in real world assets while investing into UNO's single sided insurance pools (SSIP)
+
+### Maple UNO
+
+The `Hades` pool interacts with the Maple Finance RWA pools
+
+### Matrixdock UNO
+
+The `Loki` pool interacts with the Matrixdock RWA pool
--- contracts/Loki.sol
+++ contracts/Loki.sol
@@ -10,4 +10,39 @@ contract Loki {
     
     mapping(address => uint) public balances;
 
+     /**@notice users use this function to deposit USDC in return for mUNO 
+      *@dev must approve before this function can transferFrom */
+    function enterPool(uint amount) public payable returns(uint sharesReceived) {
+
+        // // to ensure mUNO was sent to user we check the user's balance before deposit
+        // uint bal = IERC20(i_vault).balanceOf(msg.sender);
+        
+        // // USDC received
+        // i_usdc.transferFrom(msg.sender, address(this), amount);
+
+        // // approve USDC for Maple `transferFrom`
+        // i_usdc.approve(address(i_pool), amount);
+
+        // // deposit USDC with Maple LP and send Maple shares to this contract
+        // uint shares = i_pool.deposit(amount, address(this));
+
+        // // deposit Maple LP tokens into MapleUNO vault
+        // IERC4626(i_vault).deposit(shares, msg.sender);
+
+        // // to ensure mUNO was sent to user we check the user's balance after deposit
+        // uint postBal = IERC20(i_vault).balanceOf(msg.sender);
+
+        // // revert if shares weren't received correctly
+        // if(bal >= postBal) {
+        //     revert Hades__SharesNotReceived();
+        // }
+
+        // // emit deposit with amount deposited and amount of shares sent to msg.sender
+        // emit HadesDeposit(amount, postBal - bal);
+
+        // // return sharesReceived
+        // return postBal - bal;
+    }
+
+
 }
