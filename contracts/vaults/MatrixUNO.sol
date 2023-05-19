00fbdadc6c889cf2fa1af2566354b3431d840efc
Rohan <RohanZarathustra@gmail.com>
2023-05-19 14:41:30 -0400

    started on vault logic for "stake()"

--- contracts/vaults/MatrixUNO.sol
+++ contracts/vaults/MatrixUNO.sol
@@ -5,11 +5,13 @@ pragma solidity 0.8.7;
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
+error MatrixUno__ZeroAmountGiven();
+
 /**@title MatrixUno
   *@author Rohan Nero
   *@notice this contract allows UNO users to earn native STBT yields from Matrixdock.
   *@dev This vault uses STBT as the asset and xUNO as the shares token*/
-contract MapleUNO is ERC4626 {
+contract MatrixUno is ERC4626 {
 
 
     /**@notice STBT token address used as the vault asset */
@@ -19,6 +21,14 @@ contract MapleUNO is ERC4626 {
     IERC20 private constant dai = IERC20(0x6B175474E89094C44Da98b954EedeAC495271d0F);
     IERC20 private constant usdc = IERC20(0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
     IERC20 private constant usdt = IERC20(0xdAC17F958D2ee523a2206206994597C13D831ec7);
+
+    /**@notice Array of the stablecoin addresses */
+    address[3] private stables; 
+
+    /**@notice User stablecoin balances 
+      *@dev The middle uint correlates to the stables indices 
+      *@dev For example: balances[0x77][0] = DAI balance of address 0x77 */
+    mapping(address => mapping(uint => uint)) private balances;
     
 
     /**@notice need to provide the asset that is used in this vault 
@@ -26,17 +36,23 @@ contract MapleUNO is ERC4626 {
       *@param asset - the IERC contract you wish to use as the vault asset, in this case STBT*/
     constructor(address asset) ERC4626(IERC20(asset)) ERC20("Maple UNO","mUNO") {
       stbt = IERC20(asset);
+      stables = [0x6B175474E89094C44Da98b954EedeAC495271d0F,0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48,0xdAC17F958D2ee523a2206206994597C13D831ec7];
     }
 
 
-    // need function to stake stablecoins and function to return/withdrawal stablecoins
+    /** USER FUNCTIONS */
 
     /**@notice this function allows users to stake stablecoins for xUNO
       *@dev this contract holds the stablecoins and transfers xUNO from its balance
       *@param amount - the amount of stablecoin to deposit
       *@param token - the stablecoin to deposit (DAI = 0, USDC = 1, USDT = 2) 
     */
-    function stake(uint amount, uint token) public payable returns(uint shares) {
+    function stake(uint amount, uint token) public returns(uint shares) {
+      if(amount == 0) {
+        revert MatrixUno__ZeroAmountGiven();
+      }
+      IERC20(stables[token]).transferFrom(msg.sender, address(this), amount);
+      balances[msg.sender][token] += amount;
     }
 
     
