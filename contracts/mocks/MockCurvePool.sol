e9b10126de9c1bfe5daa14de6fbdc32e96152e14
Rohan <RohanZarathustra@gmail.com>
2023-05-31 12:56:58 -0400

    setting up mock curve pool

--- contracts/mocks/MockCurvePool.sol
+++ contracts/mocks/MockCurvePool.sol
@@ -2,11 +2,19 @@
 
 pragma solidity ^0.8.7;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
 /**@title Mock Curve STBT/3CRV Pool
  *@author Rohan Nero
  *@notice This is a fake version of the real STBT/3CRV pool
  *@dev This pool allows users to swap STBT for USDC */
 contract MockCurvePool {
+    /**@notice Goerli STBT contract */
+    IERC20 private stbt = IERC20(0x0f539454d2Effd45E9bFeD7C57B2D48bFd04CB32);
+
+    /**@notice Goerli USDC contract */
+    IERC20 private usdc = IERC20(0x43c7181e745Be7265EB103c5D69F1b7b4EF8763f);
+
     /**@notice Perform an exchange between two underlying coins
      *@dev need this function to allow swapping STBT into USDC
      *@param i Index value for the underlying coin to send
@@ -21,5 +29,8 @@ contract MockCurvePool {
         uint256 _dx,
         uint256 _min_dy,
         address _receiver
-    ) public returns (uint256) {}
+    ) public returns (uint256) {
+        stbt.transferFrom(msg.sender, address(this), _dx);
+        usdc.transfer(msg.sender, _min_dy);
+    }
 }
