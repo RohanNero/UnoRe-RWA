88b3a5dcc11943b7804ba8eaa30367b41ec44f9e
Shivansh070707 <Shivansh0301@gmail.com>
2023-07-24 19:18:00 +0530

    add more test

--- test/MatrixUnoMainnet.t.sol
+++ test/MatrixUnoMainnet.t.sol
@@ -239,93 +239,253 @@ contract MatrixUNOTest is Test {
         vm.stopPrank();
     }
 
-    function testDepositStableCoin() public {
-        testUnoDeposit();
-        vm.startPrank(whale);
-        //"transfers the stablecoins from user to vault"
-        uint256 initialVaultUsdcBalance = usdc.balanceOf(vault);
-        //console.log(whale)
-        uint256 usdcBalance = usdc.balanceOf(whale);
-        uint256 usdcAllowance = usdc.allowance(whale, vault);
-        //const usdcDeposit = 50000 * 1e6
-        uint256 usdcDeposit = 50000 ether;
-        console.log("whale usdc balance:", usdcBalance);
-        console.log("vault usdc allowance:", usdcAllowance);
-        console.log("initial vault usdc balance:", initialVaultUsdcBalance);
+    // function testDepositStableCoin() public {
+    //     UnoDeposit();
+    //     vm.startPrank(whale);
+    //     //"transfers the stablecoins from user to vault"
+    //     uint256 initialVaultUsdcBalance = usdc.balanceOf(vault);
+    //     //console.log(whale)
+    //     uint256 usdcBalance = usdc.balanceOf(whale);
+    //     uint256 usdcAllowance = usdc.allowance(whale, vault);
+    //     //const usdcDeposit = 50000 * 1e6
+    //     uint256 usdcDeposit = 50000 ether;
+    //     console.log("whale usdc balance:", usdcBalance);
+    //     console.log("vault usdc allowance:", usdcAllowance);
+    //     console.log("initial vault usdc balance:", initialVaultUsdcBalance);
 
-        //if (usdcAllowance < usdcDeposit)
+    //     //if (usdcAllowance < usdcDeposit)
 
-        usdc.approve(vault, usdcDeposit);
+    //     usdc.approve(vault, usdcDeposit);
 
-        uint256 updatedUsdcAllowance = usdc.allowance(whale, vault);
-        console.log("updated vault usdc allowance:", updatedUsdcAllowance);
-        uint256 totalClaimed = matrixUno.viewTotalClaimed();
+    //     uint256 updatedUsdcAllowance = usdc.allowance(whale, vault);
+    //     console.log("updated vault usdc allowance:", updatedUsdcAllowance);
+    //     uint256 totalClaimed = matrixUno.viewTotalClaimed();
 
-        //if (initialVaultUsdcBalance < usdcDeposit) {
-        uint256 shares = matrixUno.stake(usdcDeposit, 1, 99);
+    //     //if (initialVaultUsdcBalance < usdcDeposit) {
+    //     uint256 shares = matrixUno.stake(usdcDeposit, 1, 99);
 
-        uint256 finalVaultUsdcBalance = usdc.balanceOf(vault);
-        console.log("final vault usdc balance:", finalVaultUsdcBalance);
+    //     uint256 finalVaultUsdcBalance = usdc.balanceOf(vault);
+    //     console.log("final vault usdc balance:", finalVaultUsdcBalance);
 
-        //"updates the user's balance for the staked stablecoin"
-        uint256 vaultBalance = matrixUno.viewBalance(whale, 1);
-        totalClaimed = matrixUno.viewTotalClaimed();
-        console.log("whale usdc balance:", vaultBalance);
-        console.log("total claimed:", totalClaimed);
-        // eventually need to make > into = since unstake should be working properly
-        bool check = vaultBalance >= 50000000000 || totalClaimed > 0;
-        console.log(check);
-        assertTrue(check);
+    //     //"updates the user's balance for the staked stablecoin"
+    //     uint256 vaultBalance = matrixUno.viewBalance(whale, 1);
+    //     totalClaimed = matrixUno.viewTotalClaimed();
+    //     console.log("whale usdc balance:", vaultBalance);
+    //     console.log("total claimed:", totalClaimed);
+    //     // eventually need to make > into = since unstake should be working properly
+    //     bool check = vaultBalance >= 50000000000 || totalClaimed > 0;
+    //     console.log(check);
+    //     assertTrue(check);
 
-        //"transfers xUNO to the user"
-        uint256 whalexUnoBalance = matrixUno.balanceOf(whale);
-        uint256 vaultxUnoBalance = matrixUno.balanceOf(vault);
-        //string memory vaultSymbol = matrixUno.symbol();
-        totalClaimed = matrixUno.viewTotalClaimed();
-        console.log("whale xUNO balance:", whalexUnoBalance);
-        console.log("vault xUNO balance:", vaultxUnoBalance);
-        // console.log("vault shares symbol:", vaultSymbol.toString())
-        console.log("total claimed:", totalClaimed);
-        check = whalexUnoBalance > 1000 || totalClaimed > 0;
-        assertTrue(check);
-        vm.stopPrank();
-    }
+    //     //"transfers xUNO to the user"
+    //     uint256 whalexUnoBalance = matrixUno.balanceOf(whale);
+    //     uint256 vaultxUnoBalance = matrixUno.balanceOf(vault);
+    //     //string memory vaultSymbol = matrixUno.symbol();
+    //     totalClaimed = matrixUno.viewTotalClaimed();
+    //     console.log("whale xUNO balance:", whalexUnoBalance);
+    //     console.log("vault xUNO balance:", vaultxUnoBalance);
+    //     // console.log("vault shares symbol:", vaultSymbol.toString())
+    //     console.log("total claimed:", totalClaimed);
+    //     check = whalexUnoBalance > 1000 || totalClaimed > 0;
+    //     assertTrue(check);
+    //     vm.stopPrank();
+    // }
 
-    function testperformUpkeep() public {
-        testUnoDeposit();
-        //"MOCK SENDING REWARDS"
-        uint256 initialVaultAssets = stbt.balanceOf(vault);
-        uint256 thousandStbt = 1000 ether;
+    // function testperformUpkeep() public {
+    //     UnoDeposit();
+    //     //"MOCK SENDING REWARDS"
+    //     uint256 initialVaultAssets = stbt.balanceOf(vault);
+    //     uint256 thousandStbt = 1000 ether;
 
-        //if (slicedVaultAssets < 200000) {
-        vm.prank(sWhale);
-        stbt.transfer(vault, thousandStbt);
-        console.log("mock stbt rewards distributed!");
+    //     //if (slicedVaultAssets < 200000) {
+    //     vm.prank(sWhale);
+    //     stbt.transfer(vault, thousandStbt);
+    //     console.log("mock stbt rewards distributed!");
 
-        //"should update reward info for the week"
-        uint256 interval_1 = matrixUno.viewInterval();
-        uint256 initialWeek = matrixUno.viewCurrentPeriod();
-        //uint256 initialInfo = matrixUno.viewRewardInfo(initialWeek);
-        console.log("interval:", interval_1);
-        console.log("initialWeek:", initialWeek);
-        console.log(
-            "rewards, vaultAssetBalance, previousWeekBalance, claimed, currentBalance, deposited, withdrawn"
-        );
-        // console.log("initialInfo:", initialInfo);
+    //     //"should update reward info for the week"
+    //     uint256 interval_1 = matrixUno.viewInterval();
+    //     uint256 initialWeek = matrixUno.viewCurrentPeriod();
+    //     //uint256 initialInfo = matrixUno.viewRewardInfo(initialWeek);
+    //     console.log("interval:", interval_1);
+    //     console.log("initialWeek:", initialWeek);
+    //     console.log(
+    //         "rewards, vaultAssetBalance, previousWeekBalance, claimed, currentBalance, deposited, withdrawn"
+    //     );
+    //     // console.log("initialInfo:", initialInfo);
 
-        bool returnVal = matrixUno.checkUpkeep();
-        console.log("upkeepNeeded:", returnVal);
-        if (returnVal == false) {
-            vm.warp(86400);
-        }
-        matrixUno.performUpkeep();
-        //       await perform.wait(1)
-        console.log("Upkeep Performed!");
-        uint256 finalWeek = matrixUno.viewCurrentPeriod();
-        //const finalInfo = matrixUno.viewRewardInfo(initialWeek);
-        //const nextWeekInfo = matrixUno.viewRewardInfo(finalWeek);
-        console.log("finalWeek:", finalWeek);
-        // console.log("finalInfo:", finalInfo);
-        // console.log("nextWeekInfo:", nextWeekInfo);
+    //     bool returnVal = matrixUno.checkUpkeep();
+    //     console.log("upkeepNeeded:", returnVal);
+    //     if (returnVal == false) {
+    //         vm.warp(block.timestamp + 86400);
+    //     }
+    //     matrixUno.performUpkeep();
+    //     //       await perform.wait(1)
+    //     console.log("Upkeep Performed!");
+    //     uint256 finalWeek = matrixUno.viewCurrentPeriod();
+    //     //const finalInfo = matrixUno.viewRewardInfo(initialWeek);
+    //     //const nextWeekInfo = matrixUno.viewRewardInfo(finalWeek);
+    //     console.log("finalWeek:", finalWeek);
+    //     // console.log("finalInfo:", finalInfo);
+    //     // console.log("nextWeekInfo:", nextWeekInfo);
+    // }
+
+    function testViewFunctions() public {
+        UnoDeposit();
+        //"returns the curve pool address"
+        address value = matrixUno.viewPoolAddress();
+        assertEq(value, 0x892D701d94a43bDBCB5eA28891DaCA2Fa22A690b);
+
+        //"returns Uno's EOA address"
+        value = matrixUno.viewUnoAddress();
+        assertEq(value, 0x81BD585940501b583fD092BC8397F2119A96E5ba);
+
+        //"returns addresses of DAI/UDSC/USDT used by this contract"
+        address[3] memory stables = matrixUno.viewStables();
+
+        assertEq(stables[0], 0x6B175474E89094C44Da98b954EedeAC495271d0F);
+        assertEq(stables[1], 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48);
+        assertEq(stables[2], 0xdAC17F958D2ee523a2206206994597C13D831ec7);
+
+        //"returns the sanctionsList contract address"
+        value = matrixUno.viewSanctionsList();
+        assertEq(value, 0x40C57923924B5c5c5455c48D93317139ADDaC8fb);
+
+        //"returns the total stable balance of the vault"
+        uint256 bal = matrixUno.viewVaultStableBalance();
+        vm.prank(whale);
+        usdc.transfer(vault, 777);
+        uint256 updatedBal = matrixUno.viewVaultStableBalance();
+        // console.log("bal:", bal.toString())
+        // console.log("updatedBal:", updatedBal.toString())
+        assertGt(updatedBal, bal);
+
+        //"returns amount of times that users totalStaked goes into vaultAssetBalance at given week"
+        // uint256[] portion =  matrixUno.viewPortionAt(0, whale);
+        // console.log(value.toString())
+        // console.log(value[0] / 2 ** 64)
+        //assert.equal(value[0])
+
+        // describe("viewCurrentPeriod", function () {
+        //   it("returns what week the contract is currently at", async function () {
+        //     let week = 1
+        //     const value = await vault.viewCurrentPeriod()
+        //     console.log("value:", value.toString())
+        //     console.log("week:", week)
+        //     assert.equal(week, value)
+        //     week++
+        //   })
+        // })
+
+        //"returns amount of rewards a user earns"
+        (uint value0, ) = matrixUno.viewRewards(whale);
+        console.log(value0 >= 249);
+        assertGe(value0, 249);
+
+        //"returns the rewardInfo for given week"
+        uint value1 = matrixUno.viewRewardInfo(0).rewards;
+        assertGe(999999999999999999999, value1);
+
+        //"view the stablecoin balances of users"
+        // value1 = matrixUno.viewStakedBalance(whale, 1);
+        // assertEq(value1, 5e10);
+
+        //   describe("viewTotalStakedBalance", function () {
+        //     it("view total amount a user has staked", async function () {
+        //       const value = await vault.viewStakedBalance(whale.address, 1)
+        //       assert.equal(value, 5e10)
+        //     })
+        //   })
+        //   describe("viewLastClaimed", function () {
+        //     it("returns the last week a user has claimed", async function () {
+        //       const currentWeek = await vault.viewCurrentPeriod()
+        //       const value = await vault.viewLastClaimed(whale.address)
+        //       console.log(value)
+        //       assert.equal(value, Number(currentWeek) - 1)
+        //     })
+        //   })
+        //   describe("viewClaimedAmount", function () {
+        //     it("returns the amount a user has claimed", async function () {
+        //       const value = await vault.viewClaimedAmount(whale.address)
+        //       const totalClaimed = await vault.viewTotalClaimed()
+        //       assert.equal(value.toString(), totalClaimed)
+        //     })
+        //   })
+        //   describe("viewTotalClaimed", function () {
+        //     it("returns the totalClaimed variable", async function () {
+        //       const value = await vault.viewTotalClaimed()
+        //       const totalClaimed = await vault.viewClaimedAmount(whale.address)
+        //       assert.equal(value.toString(), totalClaimed)
+        //     })
+        //   })
+        //   describe("viewTotalStaked", function () {
+        //     it("returns the totalStaked variable", async function () {
+        //       const value = await vault.viewTotalStaked()
+        //       console.log(value)
+        //       const convertedAmount = value.toLocaleString("fullwide", {
+        //         useGrouping: false,
+        //       })
+        //       assert.equal(convertedAmount, 50000000000000000000000)
+        //     })
+        //   })
+        //   describe("viewUnoDeposit", function () {
+        //     it("returns the amount of STBT that Uno Re has deposited into the vault", async function () {
+        //       const value = await vault.viewUnoDeposit()
+        //       console.log(value)
+        //       const convertedAmount = value.toLocaleString("fullwide", {
+        //         useGrouping: false,
+        //       })
+        //       assert.equal(convertedAmount, 200000000000000000000000)
+        //     })
+        //   })
+        //   describe("viewStartingtime", function () {
+        //     it("returns the vault's starting timestamp", async function () {
+        //       const value = await vault.viewStartingtime()
+        //       const lastUpkeep = await vault.viewLastUpkeepTime()
+        //       assert.isBelow(value, lastUpkeep)
+        //     })
+        //   })
+        //   describe("viewLastUpkeepTime", function () {
+        //     it("returns the last time this contract had upkeep performed", async function () {
+        //       const value = await vault.viewLastUpkeepTime()
+        //       const startingTime = await vault.viewStartingtime()
+        //       assert.isAbove(value, startingTime)
+        //     })
+        //   })
+        //   describe("viewInterval", function () {
+        //     it("returns the seconds in each rewards period", async function () {
+        //       const value = await vault.viewInterval()
+        //       assert.equal(value, 86400)
+        //     })
+        //   })
+        //   describe("viewUnaccountedRewards", function () {
+        //     it("returns the amount of rewards that can be claimed by uno", async function () {
+        //       const value = await vault.viewUnaccountedRewards()
+        //       await vault.connect(sWhale).unoClaim()
+        //       const updatedValue = await vault.viewUnaccountedRewards()
+        //       // console.log(value.toString())
+        //       // console.log(updatedValue.toString())
+        //       assert.isAbove(value, updatedValue)
+        //     })
+        //   })
+        //   describe("viewStakeConversionRate", function () {
+        //     it("returns the current stake STBT / stablecoin conversion", async function () {
+        //       const value = await vault.viewStakeConversionRate()
+        //       const unstakeValue = await vault.viewUnstakeConversionRate()
+        //       const number =
+        //         (Number(value) / 2 ** 64) * (Number(unstakeValue) / 2 ** 64)
+        //       //console.log(number.toString())
+        //       assert.approximately(number, 1, 0.5)
+        //     })
+        //   })
+        //   describe("viewUnstakeConversionRate", function () {
+        //     it("returns the current unstake STBT / stablecoin conversion", async function () {
+        //       const value = await vault.viewUnstakeConversionRate()
+        //       const stakeValue = await vault.viewStakeConversionRate()
+        //       const number =
+        //         (Number(value) / 2 ** 64) * (Number(stakeValue) / 2 ** 64)
+        //       //console.log(number.toString())
+        //       assert.approximately(number, 1, 0.5)
+        //     })
+        //   })
     }
 }
