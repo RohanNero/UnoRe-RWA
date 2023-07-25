fef8595d9d344ea361e2f61df12a526e30162653
Rohan <RohanZarathustra@gmail.com>
2023-07-25 16:16:54 -0400

    rename to match repo

--- contracts/Uno/UnoRWA.sol
+++ contracts/Uno/UnoRWA.sol
@@ -6,6 +6,6 @@ pragma solidity 0.8.7;
 "import ../interfaces/ISingleSidedInsurancePool.sol";
 
 /**@notice this contract interacts with both the MatrixUno vault and its respective Uno SSIP through their interfaces */
-contract StakeRWA {
+contract UnoRWA {
 
 }
